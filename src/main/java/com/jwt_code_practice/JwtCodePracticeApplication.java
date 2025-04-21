2c24aebe0e7511a6b021aed7ba5b3e08f62c2b4f
sungyeong98 <am25227903@gmail.com>
2025-04-21 14:35:40 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/jwt_code_practice/JwtCodePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jwt_code_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtCodePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtCodePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt_code_practice/JwtCodePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt_code_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtCodePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
